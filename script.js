18ec10d84eb4a0f5baf01d314c46c6fa8d38c61d
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-07-14 01:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function display() {
+		let width=window.innerWidth;
+	let height=window.innerHeight;
+let textsize=`Width: ${width} and Height:${height}`
+
+	document.querySelector("h1").textContent=textsize
+}
+
+display()
+
+window.addEventListener("resize", display)
